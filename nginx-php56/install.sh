f6cb997c0019a94d94adaff628139735eac33848
xavren <renaudin.xavier@gmail.com>
2016-06-10 14:28:44 -0400

    add nginx php docker

---
+++ nginx-php56/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cat << 'EOT' > /etc/yum.repos.d/nginx.repo
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/centos/6/$basearch/
+gpgcheck=0
+enabled=1
+EOT
+
+yum install epel-release
+yum -y install http://rpms.remirepo.net/enterprise/remi-release-6.rpm
+
+yum -y update
+yum -y install --enablerepo=remi-php56 php-fpm \
+    php-cli \
+	php-dbg \
+	php-mbstring \
+	php-intl \
+	php-opcache \
+	php-pecl-apcu \
+	php-mcrypt \
+	php-process \
+	php-pecl-igbinary \
+	php-gmp \
+	php-xml \
+	php-pdo \
+	php-mysqlnd \
+	php-bcmath \
+	php-mongodb \
+	php-geoip
+
+yum -y install nginx
+
+yum clean all

84030db56744a139ab0d417759c1e8cd132a980c
Xavier RENAUDIN <xavier@opale.net>
2016-06-21 16:23:57 -0400

    add symfony conf and add /srv

--- nginx-php7/install.sh
+++ nginx-php7/install.sh
@@ -33,6 +33,9 @@ yum -y install nginx
 
 yum clean all
 
+mkdir -p /srv/{var,logs}
+chown nobody:nobody -R /srv
+
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === 'bf16ac69bd8b807bc6e4499b28968ee87456e29a3894767b60c2d4dafa3d10d045ffef2aeb2e78827fa5f024fbe93ca2') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
